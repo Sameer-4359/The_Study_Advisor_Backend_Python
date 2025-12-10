605af2ade5c58419cea06911baa987764802c00f
Sameer-4359 <muhammadsameer4359@gmail.com>
2025-12-11 02:44:27 +0500

    update on run.py

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
-# run.py
 import uvicorn
+import os
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8005))  # use Railway's PORT if available
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=8005,
+        port=port,
         reload=True,
         log_level="info"
     )
